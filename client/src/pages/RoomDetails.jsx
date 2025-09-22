78b0ce42ed5ba5f63e25146f000bafc11c9f34f3
Harshith917 <147698683+Harshith917@users.noreply.github.com>
2025-09-22 20:45:04 +0530

    hotelowner

--- client/src/pages/RoomDetails.jsx
+++ client/src/pages/RoomDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
-import { assets, facilityIcons, roomsDummyData } from '../assets/assets';
+import { assets, facilityIcons, roomCommonData, roomsDummyData } from '../assets/assets';
 import StarRating from '../components/StarRating';
 
 const RoomDetails = () => {
@@ -63,6 +63,75 @@ const RoomDetails = () => {
           {/* Room price */}
 
           <p className='text-2xl font-medium'>${room.pricePerNight}/night</p>
+
+        </div>
+
+
+        {/* Checkin check out */}
+        <form className='flex flex-col md:flex-row items-start md:items-center justify-between bg-white shadow-[0px_0px_20px_rgba(0,0,0,0.15)] p-6 rounded-x1 mx-auto mt-16 max-w-6xl'>
+              <div className='flex flex-col flex-wrap md:flex-row items-start md:items-center gap-4 md:gap-10 text-gray-500'>
+
+                <div className='flex flex-col'>
+                  <label htmlFor="checkInDate" className='font-medium'>check-In</label>
+                  <input type="date" id='checkInDate' placeholder='check-In' className='w-full rounded border border-gray-300 px-3 py-2 mt-1.5 outline-none' required />
+                </div>
+
+                <div className='w-px h-15 bg-gray-300/70 max-md:hidden' ></div>
+
+                <div className='flex flex-col'>
+                  <label htmlFor="checkOutDate" className='font-medium'>check-Out</label>
+                  <input type="date" id='checkOutDate' placeholder='check-Out' className='w-full rounded border border-gray-300 px-3 py-2 mt-1.5 outline-none' required />
+                </div>
+
+                <div className='w-px h-15 bg-gray-300/70 max-md:hidden' ></div>
+
+                <div className='flex flex-col'>
+                  <label htmlFor="guests" className='font-medium'>Guests</label>
+                  <input type="number" id='guests' placeholder='0' className='max-w-20 rounded border border-gray-300 px-3 py-2 mt-1.5 outline-none' required />
+                </div>
+
+                <div className='w-px h-15 bg-gray-300/70 max-md:hidden' ></div>
+
+              </div>
+              <button type='submit' className='bg-primary hover:bg-primary-dull
+              active:scale-95 transition-all text-white rounded-md max-md:w-full
+              max-md:mt-6 md:px-25 py-3 md:py-4 text-base cursor-pointer' >
+               Check Availability
+              </button>
+        </form>
+
+
+        {/* common specifications */}
+        <div className='mt-25 space-y-4'>{roomCommonData.map((spec,index)=>(
+          <div key={index} className='flex items-start gap-2'>
+            <img src={spec.icon} alt={`${spec.title}-icon`} className='w-6.5' />
+            <div>
+              <p className='text-base'>{spec.title}</p>
+              <p className='text-gray-500'>{spec.description}</p>
+            </div>
+          </div>
+        ))}</div>
+
+        <div className='max-w-3xl border-y border-gray-300 my-15 py-10 text-gray-500'>
+          <p>Guests will be allocated on the ground floor based on availability. You’ll enjoy a comfortable two-bedroom apartment with a true city vibe. The quoted price is for two guests; please select the correct number of guests at checkout to get the exact price for larger groups.</p>
+        </div>
+
+
+{/* Posted by */}
+
+        <div className='flex flex-col items-start gap-4'>
+          <div className='flex gap-4'>
+            <img src={room.hotel.owner.image} alt="Host" className='h-14 w-14 md:h-18 md:w-18 rounded-full' />
+            <div>
+              <p className='text-lg md:text-xl'>Hosted By {room.hotel.name}</p>
+              <div className='flex items-center mt-1'>
+                <StarRating />
+                <p className='ml-2'>200+ reviews</p>
+              </div>
+            </div>
+          </div>
+
+          <button className='px-6 py-2 mt-4 rounded text-white bg-primary hover:bg-primary-dull transitio-all cursor-pointer'>Contact Now</button>
 
         </div>
 
