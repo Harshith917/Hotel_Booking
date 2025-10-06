398afb7d36838b20ffb04c8f42dc66d35e4ccf55
Harshith917 <147698683+Harshith917@users.noreply.github.com>
2025-10-06 17:34:39 +0530

    client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -29,8 +29,6 @@ const App = () => {
                   <Route index element={<Dashboard />}/>
                   <Route path="add-room" element={<AddRoom />}/>
                   <Route path="list-room" element={<ListRoom />}/>
-                  
-                  
           </Route>
 
         </Routes>
